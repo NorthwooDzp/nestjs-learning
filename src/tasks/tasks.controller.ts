cfc8fb3746ac7c9cf75dd9a7e5b57bfc05f14399
NorthwooDzp <northwoodzp@gmail.com>
2019-10-02 17:32:53 +0300

    L13 done

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {
+    }
+
+    @Get()
+    private getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
     }
 }
