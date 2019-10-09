d7aa8bac1792963ea01a9804a25f001cd52ad2c2
NorthwooDzp <northwoodzp@gmail.com>
2019-10-09 11:27:13 +0300

    lesson25 ValidationPipe: Creating a Task done

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,7 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
-    public title: string;
-    public description: string;
+    @IsNotEmpty() public title: string;
+
+    @IsNotEmpty() public description: string;
 }
