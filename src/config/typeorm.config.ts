1f6796114e59c60586955e4853fc45da950bd0d7
NorthwooDzp <northwoodzp@gmail.com>
2019-10-11 17:48:24 +0300

    lesson35 Preparation for Task Service Refactoring done

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,6 +7,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     username: 'dbadmin',
     password: 'dbadmin',
     database: 'nestlearning',
-    entities: [__dirname + '../**/*.entity.ts'],
+    entities: [__dirname + '/../**/*.entity.js'],
     synchronize: true,
 };
